c14f1abd49d1a30dbb2039e2016ea240263c5c78
Keegan Neave <keegan.neave@outlook.com>
2020-07-09 21:38:45 +0100

    Update main.cpp
    Tidied up and added definitions

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,31 @@
 #include "RF24.h"
 #include <NewPing.h>
 #include <FastLED.h>
-#define NUM_LEDS 10
 
+
+//  Pin Definitions
+#define LEDPIN 7
+#define PIR0 20          // A2
+#define PIR1 21          // A3
+#define FOOTSWITCH 7
+#define SONAR1 2
+#define SONAR2 3
+#define SONAR3 4
+#define SONAR4 5
+
+#define NUM_LEDS 10
 CRGBArray<NUM_LEDS> leds;
 
 //  Sensor Setup
 #define SONAR_NUM 4      // Number of sensors.
 #define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
-#define PIR0 20          // A2
-#define PIR1 21          // A3
-#define FOOTSWITCH 7
+
 
 NewPing sonar[SONAR_NUM] = { // Sensor object array.
-    NewPing(10, 2, MAX_DISTANCE),
-    NewPing(10, 3, MAX_DISTANCE),
-    NewPing(10, 4, MAX_DISTANCE),
-    NewPing(10, 5, MAX_DISTANCE)};
+    NewPing(10, SONAR1, MAX_DISTANCE),
+    NewPing(10, SONAR2, MAX_DISTANCE),
+    NewPing(10, SONAR3, MAX_DISTANCE),
+    NewPing(10, SONAR4, MAX_DISTANCE)};
 
 //  Distance below which sonar considered triggered
 int sonarTriggerDistance[] = {10, 10, 10, 10};
@@ -35,24 +44,28 @@ int footswitchvalue = LOW;
 
 //  Radio Setup
 
-// 0 = controller, 1 = peripheral
-bool radioNumber = 1;
-RF24 radio(19, 18);
-
-byte addresses[][6] = {"1Node", "2Node"}; // Radio pipe addresses for the 2 nodes to communicate.
-
 // Role management: Set up role.  This sketch uses the same software for all the nodes
 // in this system.  Doing so greatly simplifies testing.
 typedef enum
 {
   role_controller = 0,
   role_peripheral
-} role_e;                                                                   // The various roles supported by this sketch
+} role_e;         
+                                                          // The various roles supported by this sketch
 const char *role_friendly_name[] = {"invalid", "controller", "peripheral"}; // The debug-friendly names of those roles
+
+// 0 = controller, 1 = peripheral
+bool radioNumber = 1;
+RF24 radio(19, 18);
+
+byte addresses[][6] = {"1Node", "2Node"}; // Radio pipe addresses for the 2 nodes to communicate.
+
 role_e role = role_e(radioNumber);                                          // The role of the current running sketch
 
 byte counter = 1;
 
+//  Setup variables for sharing room states
+//  1 = engaged, 2 = possibly vacant, 3 = vacant
 typedef enum
 {
   red = 1,
@@ -60,8 +73,6 @@ typedef enum
   green
 } state_e;
 
-//  Setup variables for sharing room states
-//  1 = engaged, 2 = possibly vacant, 3 = vacant
 struct RoomStates
 {
   state_e room1;
@@ -78,8 +89,7 @@ state_e sensorStates[] = {
     state_e(1),
     state_e(1),
     state_e(1),
-    state_e(1)
-};
+    state_e(1)};
 
 typedef enum
 {
@@ -108,34 +118,26 @@ void PrintRoomStates()
 
 void CheckRoomStates()
 {
-  if(sensorStates[sensor_e(room1_1)] == state_e(1) 
-  | sensorStates[sensor_e(room1_2)] == state_e(1) 
-  | sensorStates[sensor_e(pir0)] == state_e(1))
+  if ((sensorStates[sensor_e(room1_1)] == state_e(1)) | (sensorStates[sensor_e(room1_2)] == state_e(1)) | (sensorStates[sensor_e(pir0)] == state_e(1)))
   {
-      roomStateData.room1 = state_e(1);
+    roomStateData.room1 = state_e(1);
   }
-  else if(sensorStates[sensor_e(room1_1)] == state_e(2) 
-    | sensorStates[sensor_e(room1_2)] == state_e(2)
-    | sensorStates[sensor_e(pir0)] == state_e(2))
+  else if ((sensorStates[sensor_e(room1_1)] == state_e(2)) | (sensorStates[sensor_e(room1_2)] == state_e(2)) | (sensorStates[sensor_e(pir0)] == state_e(2)))
   {
-      roomStateData.room1 = state_e(2);
+    roomStateData.room1 = state_e(2);
   }
   else
   {
     roomStateData.room1 = state_e(3);
   }
-    
-  if(sensorStates[sensor_e(room2_1)] == state_e(1) 
-  | sensorStates[sensor_e(room2_2)] == state_e(1)
-  | sensorStates[sensor_e(pir1)] == state_e(1))
+
+  if ((sensorStates[sensor_e(room2_1)] == state_e(1)) | (sensorStates[sensor_e(room2_2)] == state_e(1)) | (sensorStates[sensor_e(pir1)] == state_e(1)))
   {
-      roomStateData.room2 = state_e(1);
+    roomStateData.room2 = state_e(1);
   }
-  else if(sensorStates[sensor_e(room2_1)] == state_e(2) 
-  | sensorStates[sensor_e(room2_2)] == state_e(2)
-  | sensorStates[sensor_e(pir1)] == state_e(2))
+  else if ((sensorStates[sensor_e(room2_1)] == state_e(2)) | (sensorStates[sensor_e(room2_2)] == state_e(2)) | (sensorStates[sensor_e(pir1)] == state_e(2)))
   {
-      roomStateData.room2 = state_e(2);
+    roomStateData.room2 = state_e(2);
   }
   else
   {
@@ -154,21 +156,18 @@ void CheckSensorStates()
     {
       //  Set state to red
       sensorStates[i] = state_e(1);
-    } 
-    else if(timeSinceTriggered < greenTrigger)
+    }
+    else if (timeSinceTriggered < greenTrigger)
     {
       //  Set state to amber
-      sensorStates[i] = state_e(2);  
+      sensorStates[i] = state_e(2);
     }
     else
     {
       //  Set state to green
       sensorStates[i] = state_e(3);
-    }    
+    }
   }
-
-  
-
 }
 
 void ReadSensors()
@@ -208,13 +207,106 @@ void ReadSensors()
   }
 }
 
+void UpdateRadio()
+{
+  if (radioNumber == 0)
+  {
+    byte gotByte; // Initialize a variable for the incoming response
+
+    radio.stopListening(); // First, stop listening so we can talk.
+
+    if (radio.write(&roomStateData, sizeof(roomStateData)))
+    { // Send the counter variable to the other radio
+      if (!radio.available())
+      {
+        //  Set status LED to red
+      }
+      else
+      {
+        while (radio.available())
+        {                          // If an ack with payload was received
+          radio.read(&gotByte, 1); // Read it, and display the response time
+          counter++; // Increment the counter variable
+        }
+      }
+    }
+    else
+    {
+      Serial.print(millis());
+      Serial.println(F(": sending failed.")); // If no ack response, sending failed
+      delay(1000);
+    }
+  }
+  else
+  {
+    //  Radio Number 2
+    byte pipeNo, gotByte; // Declare variables for the pipe and the byte received
+    while (radio.available(&pipeNo))
+    { // Read all available payloads
+      radio.read(&roomStateData, sizeof(roomStateData));
+      // Since this is a call-response. Respond directly with an ack payload.
+      gotByte += 1; // Ack payloads are much more efficient than switching to transmit mode to respond to a call
+      // radio.writeAckPayload(pipeNo, &gotByte, 1); // This can be commented out to send empty payloads.
+      Serial.print(F("Loaded response "));
+      Serial.print(roomStateData.room1);
+      Serial.print(",");
+      Serial.println(roomStateData.room2);
+    }
+  }
+}
+
+void InitLights()
+{
+  FastLED.addLeds<WS2812B, LEDPIN, RGB>(leds, NUM_LEDS);
+}
+
+void SetRoomLEDs(state_e state, int start)
+{
+  switch (state)
+  {
+  case state_e(green):
+    leds[start] = CRGB::Green;
+    leds[start + 1] = CRGB::Black;
+    leds[start + 2] = CRGB::Black;
+    break;
+  case state_e(amber):
+    leds[start] = CRGB::Black;
+    leds[start + 1] = CRGB::Yellow;
+    leds[start + 2] = CRGB::Black;
+    break;
+  case state_e(red):
+    leds[start] = CRGB::Black;
+    leds[start + 1] = CRGB::Black;
+    leds[start + 2] = CRGB::Red;
+    break;
+  }
+}
+
+void UpdateLEDS()
+{
+  //  Status
+  //  TODO:  Set logic so show RAG based on time since last radio packet
+  leds[0] = CRGB::Red;
+
+  //  Alley
+  SetRoomLEDs(roomStateData.alley_front, 1);
+
+  //  Room 1
+  SetRoomLEDs(roomStateData.room1, 4);
+
+  //  Room 2
+  SetRoomLEDs(roomStateData.room2, 7);
+
+  FastLED.show();
+}
+
 void setup()
 {
   Serial.begin(115200);
 
   //  LED init
-  FastLED.addLeds<NEOPIXEL,7>(leds, NUM_LEDS);
-
+  InitLights();
+  
   //  Sensor init
   pinMode(PIR0, INPUT);
   pinMode(PIR1, INPUT);
@@ -250,80 +342,14 @@ void setup()
 
 void loop()
 {
-  // static uint8_t hue;
-  // for(int i = 0; i < NUM_LEDS/2; i++) {   
-  //   // fade everything out
-  //   leds.fadeToBlackBy(40);
-
-  //   // let's set an led value
-  //   leds[i] = CHSV(hue++,255,255);
-
-  //   // now, let's first 20 leds to the top 20 leds, 
-  //   leds(NUM_LEDS/2,NUM_LEDS-1) = leds(NUM_LEDS/2 - 1 ,0);
-  //   FastLED.delay(33);
-  // }
-
   //  If 1, access requested
   footswitchvalue = digitalRead(FOOTSWITCH);
 
-  if (radioNumber == 0)
-  {
-    byte gotByte; // Initialize a variable for the incoming response
-
-    radio.stopListening(); // First, stop listening so we can talk.
-    // Serial.print(F("Now sending ")); // Use a simple byte counter as payload
-    // Serial.println(counter);
-
-    unsigned long time = micros(); // Record the current microsecond count
-
-    if (radio.write(&roomStateData, sizeof(roomStateData)))
-    { // Send the counter variable to the other radio
-      if (!radio.available())
-      { 
-        //  Set status LED to red
-      }
-      else
-      {
-        while (radio.available())
-        {                          // If an ack with payload was received
-          radio.read(&gotByte, 1); // Read it, and display the response time
-          unsigned long timer = micros();
-
-          // Serial.print(F("Got response "));
-          // Serial.print(gotByte);
-          // Serial.print(F(" round-trip delay: "));
-          // Serial.print(timer - time);
-          // Serial.println(F(" microseconds"));
-          counter++; // Increment the counter variable
-        }
-      }
-
-      ReadSensors();
-      CheckSensorStates();
-      CheckRoomStates();
-      PrintRoomStates();
-    }
-    else
-    {
-      Serial.print(millis());
-      Serial.println(F(": sending failed.")); // If no ack response, sending failed
-      delay(1000);
-    }
-  }
-  else
-  {
-    //  Radio Number 2
-    byte pipeNo, gotByte; // Declare variables for the pipe and the byte received
-    while (radio.available(&pipeNo))
-    { // Read all available payloads
-      radio.read(&roomStateData, sizeof(roomStateData));
-      // Since this is a call-response. Respond directly with an ack payload.
-      gotByte += 1;                               // Ack payloads are much more efficient than switching to transmit mode to respond to a call
-      // radio.writeAckPayload(pipeNo, &gotByte, 1); // This can be commented out to send empty payloads.
-      Serial.print(F("Loaded response "));
-      Serial.print(roomStateData.room1);
-      Serial.print(",");
-      Serial.println(roomStateData.room2);
-    }
-  }
+  ReadSensors();
+  CheckSensorStates();
+  CheckRoomStates();
+  PrintRoomStates();
+  
+  UpdateLEDS();
+  UpdateRadio();
 }
